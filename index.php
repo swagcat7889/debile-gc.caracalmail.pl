9ec18fc8876a1449b7c6e3d8dbc4e1fa540929da
cola cat <86305856+swagcat7889@users.noreply.github.com>
2021-08-12 13:50:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if(strlen(strstr($_SERVER['HTTP_USER_AGENT'],"warlike-gun-fence-7fjs6")) <= 0 ){ // if not debile-gc webapp
+if(strlen(strstr($_SERVER['HTTP_USER_AGENT'],"warlike-gun-fence-7fjs6")) <= 1 ){ // if not debile-gc webapp
   include("site.html");
 } else {
   include("webapp.html");
